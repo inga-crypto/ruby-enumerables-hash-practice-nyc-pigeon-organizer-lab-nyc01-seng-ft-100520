e34fb2416de5a1145d50647cf59c5d1a9f27f86f
Inga <ijaanimets@gmail.com>
2020-10-01 04:16:48 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,4 +1,16 @@
 def nyc_pigeon_organizer(data)
-  data.collect {|key, value| value }
+  reults = data.each_with_object({}) do |key, value| final_arr|
+    value.each do |k, names |
+      names.each do |name|
+        if !final_arr[name]
+          final_arr[name] = {}
+        end
+        if !final_arr[name][key]
+          !final_arr[name][key] = []
+        end
+        final_arr[name][key].push(k.to_s)
+      end
+    end
+  end
   
 end
